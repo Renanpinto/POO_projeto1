d91a36ae90f88248f831ae5972e8885a3a56953e
Bllac <Bllac@DESKTOP-77B98UT>
2017-08-28 20:57:25 -0300

    Foi elaborado o Servlet Juros Simples por completo.

--- src/java/br/com/fatecpg/web/poo1/JurosSimples.java
+++ src/java/br/com/fatecpg/web/poo1/JurosSimples.java
@@ -34,17 +34,63 @@ public class JurosSimples extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
+                        out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet JurosSimples</title>");            
+            out.println("<title>Servlet Juros Simples</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1><a href=\"Home\">Voltar</a></h1>");
+            out.println("<h1> Juros Simples </h1>");
+            out.println("<form method='get' action='JurosSimples'>");
+            out.println("<p> Valor da Aplicação ou Dívida: </p>");
+            out.println("<input type='number' name='p' step='any' min='0' />");
+            out.println("<p> Juros ( Em porcentagem): </p>");
+            out.println("<input type='number' name='i' step='any' min='0' />");
+            out.println("<p> Tempo (Meses): </p>");
+            out.println("<input type='number' name='n' step='any' min='0' />");
+            out.println("<br>"); out.println("<br>");
+            out.println("<Input type='submit' value='calcular'/>");
+            out.println("</form>");
+            
+            
+            double p,i,n,m,j ;
+            
+            
+            
+            if(request.getParameter("p")!= null) {
+                try {
+                    p = Double.parseDouble(request.getParameter("p"));
+                    i = Double.parseDouble(request.getParameter("i"));
+                    n = Double.parseDouble(request.getParameter("n"));
+                    
+                    j = (p*i*n)/100;
+                    m = j+p;
+                    
+                    out.printf("<h4> valor do juros é: R$"+j+"</h4>");
+                    out.printf("<h4> valor do montante é: R$"+m+"</h4>");
+                    
+                }
+                
+                
+                
+                 
+            catch(Exception ex){
+                out.println("<h4>Parâmetros Inválidos</h4>");
+            }
+                                   
+            }
+                            
+            
+            
+                    
+            
+            
+            
             out.println("</body>");
             out.println("</html>");
         }
     }
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -84,5 +130,23 @@ public class JurosSimples extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private static class DecimalFormat {
+
+        public DecimalFormat() {
+        }
+
+        private DecimalFormat(String string) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        private String format(double j) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        private void applyPattern(String r$_000) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
 
 }
